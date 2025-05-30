f5550e4935b7a1328a3a86fd4846866f67bb553e
Daijohn Bryant <ohnntsendto@gmail.com>
2025-05-29 23:49:43 -0400

    Add files via upload

---
+++ score_board/index.js
@@ -0,0 +1,87 @@
+let score = 0;
+
+
+
+
+
+function add1(){
+score += 1;
+document.getElementsByClassName("number1")[0].textContent = score 
+
+}
+
+
+function add2(){
+score += 2;
+document.getElementsByClassName("number1")[0].textContent = score 
+
+}
+
+
+function add3(){
+score += 3;
+document.getElementsByClassName("number1")[0].textContent = score 
+play
+
+
+}
+
+
+
+
+function add1(){
+score += 1;
+document.getElementById('homeScore').textContent = score 
+var audio = document.getElementById("Swish");
+audio.play();
+
+}
+
+
+function add2(){
+score += 2;
+document.getElementById('homeScore').textContent = score 
+var audio = document.getElementById("Swish");
+audio.play(); 
+
+}
+
+
+function add3(){
+score += 3;
+document.getElementById('homeScore').textContent = score 
+var audio = document.getElementById("Swish");
+audio.play();
+
+}
+
+
+function add1(){
+score += 1;
+document.getElementById('guestScore').textContent = score 
+var audio = document.getElementById("Swish");
+audio.play();
+
+}
+
+
+function add2(){
+score += 2;
+document.getElementById("guestScore").textContent = score
+var audio = document.getElementById("Swish");
+audio.play(); 
+
+}
+
+
+function add3(){
+score += 3;
+document.getElementById('guestScore').textContent = score 
+var audio = document.getElementById("Swish");
+audio.play();
+
+}
+
+
+
+
